0c7320496e00a602f6280ef3b7b17d006e96e537
Student <Student@205-37>
2023-10-24 14:55:11 +0800

    加入專案檔案。

---
+++ kidApp1024/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kidApp1024
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ kidApp1024/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kidApp1024
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
